88dff4f36b31df26e06e1a99deb1907472e40c27
anastasya-777 <152620467+anastasya-777@users.noreply.github.com>
2024-05-29 14:47:37 +0300

    Update MonthServiceTest.java

--- src/test/java/MonthServiceTest.java
+++ src/test/java/MonthServiceTest.java
@@ -1,2 +1,16 @@
-package PACKAGE_NAME;public class MonthServiceTest {
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+
+
+public class MonthServiceTest {
+    @ParameterizedTest
+    @CsvFileSource(files = "src/test/resources/month.csv")
+    public void testMonth(int income, int expenses, int threshold, int expected) {
+        MonthService service = new MonthService();
+        int actual = service.calculate(income, expenses, threshold);
+        Assertions.assertEquals(expected, actual);
+
+
+    }
 }
