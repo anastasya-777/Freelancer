6859c805e05d900d15d276c51ca35a36431e96bc
anastasya-777 <152620467+anastasya-777@users.noreply.github.com>
2024-05-29 14:46:07 +0300

    Update MonthService.java

--- src/main/java/MonthService.java
+++ src/main/java/MonthService.java
@@ -1,2 +1,17 @@
-package PACKAGE_NAME;public class MonthService {
+public class MonthService {
+    public int calculate(int income, int expenses, int threshold) {
+        int count = 0;
+        int money = 0;
+
+        for (int i = 0; i < 12; i++) {
+            if (money >= threshold) {
+                count++;
+                money = (money - expenses) / 3;
+            } else {
+                money += income - expenses;
+            }
+        }
+        return count;
+    }
+
 }
